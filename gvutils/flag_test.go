3c138247e5b459fdddbdb1c58a95e09a3507d5de
George Spiceland <georgespiceland@gmail.com>
2016-08-20 13:08:18 -0400

    Added test for current flags

--- gvutils/flag_test.go
+++ gvutils/flag_test.go
@@ -1,15 +1,10 @@
 package gvutils
 
-import (
-	"testing"
-)
+import ()
 
-func TestVersionFlag(t *testing.T) {
-	expectedStr := "Current Version is 0.0, built at 2016-08-20T12:30:00-0400"
+func ExampleVersionFlag() {
 	Version = "0.0"
 	BuildTime = "2016-08-20T12:30:00-0400"
-	result := DisplayVersion()
-	if result != expectedStr {
-		t.Fatalf("Expected result to be:\n%s, but got instead:\n%s\n", expectedStr, result)
-	}
+	DisplayVersion()
+	// Output: Current Version is 0.0, built at 2016-08-20T12:30:00-0400.
 }
