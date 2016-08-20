4e9c63abb29507fcd07aa73058d400bfcf24eb9d
George Spiceland <georgespiceland@gmail.com>
2016-08-20 11:49:00 -0400

    Created a generic type for Media of all kinds

---
+++ mediatype/mediatype.go
@@ -0,0 +1,10 @@
+package mediatype
+
+import ()
+
+//This is a generic struct for holding things that are the same between different types
+type media struct {
+	mediaName string
+	mediaType string
+	mediaSize int64
+}
