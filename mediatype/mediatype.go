b224c49b3096561398708d157561811f1fea0c60
George Spiceland <georgespiceland@gmail.com>
2016-08-21 00:55:53 -0400

    This closes #12 by correcting the comment structure

--- mediatype/mediatype.go
+++ mediatype/mediatype.go
@@ -2,7 +2,7 @@ package mediatype
 
 import ()
 
-//This is a generic struct for holding things that are the same between different types
+//Media is a generic struct for holding things that are the same between different types
 type Media struct {
 	mediaName string
 	mediaType string
