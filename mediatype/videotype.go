28f5805feb02662da5210cd0f2dc8c60bca8d0fb
George Spiceland <georgespiceland@gmail.com>
2016-08-21 01:10:25 -0400

    This closes #25, closes #26, closes #27, closes #28, closes #29 fixing issues with GoLint required commentary

--- mediatype/videotype.go
+++ mediatype/videotype.go
@@ -2,24 +2,29 @@ package mediatype
 
 import ()
 
+//Video is a struct that builds on the Media struct adding new attributes for Videos.
 type Video struct {
 	Media
 	mediaFormat  string
 	mediaRunTime string
 }
 
+//MediaFormat is a getter function that returns Video.mediaFormat.
 func (v *Video) MediaFormat() string {
 	return v.mediaFormat
 }
 
+//MediaRunTime is a getter function that returns Video.mediaRunTime.
 func (v *Video) MediaRunTime() string {
 	return v.mediaRunTime
 }
 
+//SetMediaFormat is a setter function that takes a string and assigns it to Video.mediaFormat
 func (v *Video) SetMediaFormat(newMediaFormat string) {
 	v.mediaFormat = newMediaFormat
 }
 
+//SetMediaRunTime is a setter function that takes a string and assigns it to Video.mediaRunTime
 func (v *Video) SetMediaRunTime(newMediaRunTime string) {
 	v.mediaRunTime = newMediaRunTime
 }
