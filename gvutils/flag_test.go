317af6e10afddac635a8379c5c87ddb3f66428ff
George Spiceland <georgespiceland@gmail.com>
2016-08-21 00:52:44 -0400

    This closes #10 fixing GoLint error messages

--- gvutils/flag_test.go
+++ gvutils/flag_test.go
@@ -2,6 +2,7 @@ package gvutils
 
 import ()
 
+//ExampleVersionFlag is an example run of DisplayVersion to test it's output
 func ExampleVersionFlag() {
 	Version = "0.0"
 	BuildTime = "2016-08-20T12:30:00-0400"
